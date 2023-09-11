7dc2de18dac5071194d20abc3f432c738f846f9f
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2023-09-11 16:32:25 +0100

    adds tailwind and tailwind forms plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Form Testing Practice</h1>;
+  return (
+    <main className="p-4 max-w-[48rem] mx-auto">
+      <h1 className="text-3xl font-bold mb-8">Form Testing Practice</h1>
+    </main>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import forms from "@tailwindcss/forms";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [forms],
+};
