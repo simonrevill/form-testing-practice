50f8752266312fa1097124ea33bfbcfe0b276b03
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2023-09-15 20:11:49 +0100

    adds container for RegistrationForm

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import RegistrationForm from "./forms/RegistrationForm";
 function App() {
   return (
     <main className="min-h-screen p-4 md:p-8">
-      <h2 className="mb-4 text-4xl font-bold">Registration</h2>
-      <RegistrationForm />
+      <div className="mx-auto max-w-[48rem]">
+        <h2 className="mb-4 text-4xl font-bold">Registration</h2>
+        <RegistrationForm />
+      </div>
     </main>
   );
 }
