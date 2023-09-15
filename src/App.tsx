f00fb845505134934a8f29d3268a140881eab7d6
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2023-09-15 20:09:32 +0100

    adds form title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import RegistrationForm from "./forms/RegistrationForm";
 
 function App() {
   return (
-    <main className="p-4 md:p-8 min-h-screen">
+    <main className="min-h-screen p-4 md:p-8">
+      <h2 className="mb-4 text-4xl font-bold">Registration</h2>
       <RegistrationForm />
     </main>
   );
